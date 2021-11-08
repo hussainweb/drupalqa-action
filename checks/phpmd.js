d51ddb17f5fb657058e8c6a7199f499bdf4e8e7e
hw <hussainweb@gmail.com>
2021-11-08 16:55:57 -0500

    feat(phpmd): Add PHPMD support

--- checks/phpmd.js
+++ checks/phpmd.js
@@ -1,4 +1,11 @@
 module.exports = ((options, webRoot) => {
   const commandArray = ['phpmd'];
+  commandArray.push(options.path ? options.path : webRoot + '/modules/custom');
+  commandArray.push(options.format ? options.format : 'text');
+  commandArray.push(options.ruleset ? options.ruleset : 'codesize,naming,unusedcode');
+  commandArray.push('--suffixes', options.suffixes ? options.suffixes : 'php,module,theme,engine,inc');
+  if (options.exclude) {
+    commandArray.push('--exclude', options.exclude);
+  }
   return commandArray;
 });
