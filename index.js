4056c6853ae16c4e87fa40edc1c7f48236803722
hw <hussainweb@gmail.com>
2021-11-08 12:33:01 -0500

    Try out the action with just PHPCS for now

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+const { exec } = require('@actions/exec');
 
-try {
-  
-} catch (error) {
-  core.setFailed(error.message);
+async function main() {
+  const phpVersion = core.getInput('php-version');
+  if (!['7.3', '7.4', '8.0', 'latest'].includes(phpVersion)) {
+    throw new Error("Invalid PHP version.");
+  }
+  const versionString = phpVersion == 'latest' ? 'latest' : 'php' + phpVersion;
+  const dockerImage = 'hussainweb/drupalqa:' + versionString;
+
+  const failFast = core.getInput('fail-fast');
+  const webRoot = core.getInput('web-root');
+
+  const env = { ...process.env };
+
+  const commonDockerOptions = [
+    '--workdir', env.GITHUB_WORKSPACE,
+    '--rm',
+    '-e', 'CI',
+    '-e', 'GITHUB_ACTION',
+    '-v', '/var/run/docker.sock:/var/run/docker.sock',
+    '-v', env.GITHUB_WORKSPACE + ':' + env.GITHUB_WORKSPACE,
+  ];
+
+  const phpCsCommand = ['phpcs', '--standard=phpcs.xml.dist', '--extensions=php,module,inc,install,test,profile,theme', '--ignore=/node_modules/', webRoot + '/modules/custom'];
+
+  await exec(
+    'docker',
+    [ 'run', ...commonDockerOptions, dockerImage, phpCsCommand ],
+  );
 }
+
+main().catch(err => {
+  core.setFailed(err.message);
+});
