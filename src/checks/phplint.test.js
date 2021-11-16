807cf599897e6b63b62bddc3843320cc9d3e58b3
hw <hussainweb@gmail.com>
2021-11-15 23:32:29 -0500

    test: add tests for phplint. See #3

---
+++ src/checks/phplint.test.js
@@ -0,0 +1,37 @@
+const phplint = require('./phplint');
+
+test('it returns defaults', () => {
+  expect(phplint({}, 'web')).toEqual(['phplint', '--exclude=vendor', '--exclude=web/core', '--exclude=web/modules/contrib', '--extensions=php,module,theme,engine,inc,install']);
+});
+
+test('it returns no options', () => {
+  expect(phplint({ no_default_options: true }, 'web')).toEqual(['phplint']);
+  expect(phplint({
+    no_default_options: true,
+    exclude: 'vendor',
+  }, 'web')).toEqual(['phplint']);
+});
+
+test('it handles partial inputs', () => {
+  let command;
+  command = phplint({
+    exclude: 'vendor,web/themes/contrib',
+  }, 'web');
+  expect(command).toContain('--exclude=vendor');
+  expect(command).toContain('--exclude=web/themes/contrib');
+  expect(command).not.toContain('--exclude=web/modules/contrib');
+
+  command = phplint({
+    extensions: 'php,inc',
+  }, 'docroot');
+  expect(command).toContain('--extensions=php,inc');
+  expect(command).toContain('--exclude=docroot/modules/contrib');
+
+  command = phplint({
+    verbose: true,
+    path: 'web/modules/custom',
+  }, 'docroot');
+  expect(command).toContain('-v');
+  expect(command).toContain('--exclude=docroot/modules/contrib');
+  expect(command).toContain('web/modules/custom');
+});
