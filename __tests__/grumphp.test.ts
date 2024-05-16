d3993b13af0a0ffb483712211c3ed05c568fdeb0
hw <hussainweb@gmail.com>
2024-05-16 10:42:36 -0400

    feat: add grumphp check
    fixes #118

---
+++ __tests__/grumphp.test.ts
@@ -0,0 +1,31 @@
+import grumphp from '../src/checks/grumphp'
+import {expect, test} from '@jest/globals'
+
+test('it returns defaults', () => {
+  expect(grumphp({}, 'web')).toEqual([
+    'grumphp',
+    'run'
+  ])
+})
+
+test('it handles partial inputs', () => {
+  let command
+  command = grumphp(
+    {
+      testsuite: 'linters',
+    },
+    'web'
+  )
+  expect(command).toContain('--testsuite=linters')
+
+  command = grumphp(
+    {
+      tasks: [
+        'phpcs',
+        'phpmd'
+      ]
+    },
+    'docroot'
+  )
+  expect(command).toContain('--tasks=phpcs,phpmd')
+})
